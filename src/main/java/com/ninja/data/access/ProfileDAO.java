6174aeb811156fb0d719c55d587d430b970cff44
Akshay Bharadia <akshaybharadia@gmail.com>
2013-09-19 00:57:47 +0530

    Adding code for REST service for simple data access

---
+++ src/main/java/com/ninja/data/access/ProfileDAO.java
@@ -0,0 +1,10 @@
+package com.ninja.data.access;
+
+/*
+ * Interface for DAO to access Profile related information
+ */
+
+public interface ProfileDAO {
+	public void create(Profile p);
+	public Profile get(int id);
+}
